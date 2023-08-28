2f36407d0f4777b3be4b161a090f9827918f8a13
Hannes Lowette <lowette.hannes@gmail.com>
2023-08-28 11:30:56 +0200

    Some Value types & entities

--- source/BeerSender/BeerSender.Domain/Boxes/Box.cs
+++ source/BeerSender/BeerSender.Domain/Boxes/Box.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BeerSender.Domain.Boxes
+﻿namespace BeerSender.Domain.Boxes
 {
     public class Box
     {
+        public Box(Box_capacity capacity)
+        {
+            Capacity = capacity;
+        }
+        public Box_capacity Capacity { get; }
 
+        public Shipping_label Label { get; set; }
+
+        public List<(Beer_bottle Bottle, int Quantity)> Contents { get; } =
+            new ();
 
     }
 
@@ -27,6 +29,27 @@ namespace BeerSender.Domain.Boxes
         }
     }
 
+    public record Beer_bottle(string Brewery, string Name, decimal Alcohol_percentage);
 
+    public record Shipping_label(Carrier Carrier, string Tracking_code)
+    {
+        public bool Is_valid()
+        {
+            return Carrier switch
+            {
+                Carrier.UPS => Tracking_code.StartsWith("ABC"), // UPS logic
+                Carrier.FedEx => Tracking_code.StartsWith("DEF"), // UPS logic
+                Carrier.PostNord => Tracking_code.StartsWith("GHI"), // UPS logic
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+    }
+
+    public enum Carrier
+    {
+        UPS,
+        FedEx,
+        PostNord
+    }
 
 }
