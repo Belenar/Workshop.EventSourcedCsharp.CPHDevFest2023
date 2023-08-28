4ec3d496f7ef9a54c425362c792c6705423e09a4
Hannes Lowette <lowette.hannes@gmail.com>
2023-08-28 11:16:06 +0200

    Box capacity

--- source/BeerSender/BeerSender.Domain/Boxes/Box.cs
+++ source/BeerSender/BeerSender.Domain/Boxes/Box.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BeerSender.Domain.Boxes
 {
-    internal class Box
+    public class Box
     {
+
+
     }
+
+    public record Box_capacity(int Number_of_spots)
+    {
+        public static Box_capacity From_number_of_spots(int number_of_spots)
+        {
+            var capacity = number_of_spots switch
+            {
+                <= 6 => new Box_capacity(6),
+                <= 12 => new Box_capacity(12),
+                _ => new Box_capacity(24)
+            };
+            return capacity;
+        }
+    }
+
+
+
 }
