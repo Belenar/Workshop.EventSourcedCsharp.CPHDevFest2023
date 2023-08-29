81e2826affe7023302070874d5aa347bb0a077c4
Hannes Lowette <lowette.hannes@gmail.com>
2023-08-29 11:02:19 +0200

    Added row_version

--- source/BeerSender/BeerSender.Web/Event_store/Event_context.cs
+++ source/BeerSender/BeerSender.Web/Event_store/Event_context.cs
@@ -16,6 +16,8 @@ public class Event_context : DbContext
 
         event_config.HasKey(a => new { a.Aggregate_id, a.Event_Number });
 
+        event_config.HasIndex(a => a.Row_version);
+
         event_config.Property(a => a.Event_Type)
             .HasMaxLength(256)
             .HasColumnType("varchar");
@@ -23,6 +25,9 @@ public class Event_context : DbContext
         event_config.Property(a => a.Event_Payload)
             .HasMaxLength(2048)
             .HasColumnType("varchar");
+
+        event_config.Property(a => a.Row_version)
+            .IsRowVersion();
 
         event_config.Ignore(a => a.Event);
     }
@@ -34,6 +39,7 @@ public class Aggregate_event
     public int Event_Number { get; set; }
     public string Event_Type { get; set;}
     public string Event_Payload { get; set;}
+    public byte[] Row_version { get; set; }
 
     private object? _event;
     public object Event
---
+++ source/BeerSender/BeerSender.Web/Migrations/20230829085922_AddRowVersion.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeerSender.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRowVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Row_version",
+                table: "Events",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_Row_version",
+                table: "Events",
+                column: "Row_version");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Events_Row_version",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Row_version",
+                table: "Events");
+        }
+    }
+}
